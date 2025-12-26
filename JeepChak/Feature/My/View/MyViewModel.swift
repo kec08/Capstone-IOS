9117f64f8b5b6b21af50e1b3414a85a946ddf2a1
kimeunchan <kec4489@naver.com>
2025-12-27 00:54:18 +0900

    fix: My 페이지 로그아웃 appState.isLoggedIn = false로 전환하여 로그아웃 처리 구현

--- JeepChak/Feature/My/View/MyViewModel.swift
+++ JeepChak/Feature/My/View/MyViewModel.swift
@@ -9,27 +9,19 @@ import SwiftUI
 import Combine
 
 final class MyViewModel: ObservableObject {
-    private let appState: AppState
     @Published var userInfo: UserInfo
     @Published var showSettings: Bool = false
 
-    init(appState: AppState) {
-            self.appState = appState
-            
-            userInfo = UserInfo(
-                name: "김은찬",
-                email: "Kkk1234@naver.com",
-                phone: "010-1234-5678",
-                userId: "kimeunchan"
-            )
-        }
+    init() {
+        userInfo = UserInfo(
+            name: "김은찬",
+            email: "Kkk1234@naver.com",
+            phone: "010-1234-5678",
+            userId: "kimeunchan"
+        )
+    }
 
     func openSettings() {
         showSettings = true
-    }
-
-    func logout() {
-        appState.logout()
-        print("로그아웃 실행")
     }
 }
